c0107b3eb2e66315389f095e49c52e39652307ab
Bohdan Marko <goldenbohdanm@gmail.com>
2024-04-17 13:27:12 +0300

    Implement a scraper with pagination

--- scraper/spiders/realtylink.py
+++ scraper/spiders/realtylink.py
@@ -1,7 +1,12 @@
+from typing import Any
+from urllib.parse import urljoin
+
 import scrapy
 import json
 
-from urllib.parse import urljoin
+from scrapy.selector import Selector
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class RealtyLinkSpider(scrapy.Spider):
@@ -9,30 +14,48 @@ class RealtyLinkSpider(scrapy.Spider):
     start_urls = [
         "https://realtylink.org/en/properties~for-rent/",
     ]
+    BASE_URL = "https://realtylink.org/en/properties~for-rent/"
 
-    BASE_URL = "https://realtylink.org/"
+    def __init__(self, **kwargs: Any):
+        super().__init__(**kwargs)
+        self.driver = webdriver.Chrome()
 
     def parse(
         self, response: scrapy.http.Response, **kwargs
     ) -> scrapy.Request:
-        pass
+        self.driver.get(response.url)
+        selector = Selector(text=self.driver.page_source)
+
+        yield from self._parse_page(selector)
+
+        next_button = self.driver.find_element(By.CSS_SELECTOR, "li.next a")
+        for _ in range(5):
+            if next_button:
+                next_button.click()
+                selector = Selector(text=self.driver.page_source)
+                yield from self._parse_page(selector)
 
     def _parse_page(
-        self, response: scrapy.http.Response, **kwargs
+        self,
+        response: Selector,
     ) -> scrapy.Request:
-        pass
+        urls = response.css("div.shell a.a-more-detail::attr('href')").getall()
+
+        for url in urls:
+            yield scrapy.Request(
+                url=urljoin(self.BASE_URL, url), callback=self._parse_item
+            )
 
     def _parse_item(self, response: scrapy.http.Response) -> dict:
-        # TODO 1: Find the date published or updated
-        url = response.css("span[id='SummaryUrl']::text").get()
-        link_on_ad = urljoin(self.BASE_URL, url)
+        link_on_ad = response.url
         title = response.css("span[data-id='PageTitle']::text").get()
         full_address = (
             response.css("h2[itemprop='address']::text").get().strip()
         )
         region = full_address.split(",", 1)[-1].strip()
-        description = (
-            response.css("div[itemprop='description']::text").get().strip()
+        description = response.css("div[itemprop='description']::text").get()
+        description_stripped = (
+            description.strip() if description else "No provided description"
         )
         price = response.css(
             "div.price[itemprop='offers'] meta[itemprop='price']::attr(content)"
@@ -47,15 +70,17 @@ class RealtyLinkSpider(scrapy.Spider):
         )
         bedrooms = response.css("div.row.teaser div.cac::text").get()
         num_of_rooms = int(bedrooms.strip().split()[0]) if bedrooms else 1
-
         return {
             "link_on_add": link_on_ad,
             "title": title,
             "region": region,
             "full_address": full_address,
-            "description": description,
+            "description": description_stripped,
             "price": price,
             "floor_area": floor_area,
             "image_links": image_links,
             "num_of_rooms": num_of_rooms,
         }
+
+    def close(self, spider, reason):
+        self.driver.quit()
